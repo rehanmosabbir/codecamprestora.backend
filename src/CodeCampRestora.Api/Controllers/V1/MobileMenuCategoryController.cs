9b1c1e1f57425b0ae55854d10fc8bd7b41e89465
muhit <sahid.299350@gmail.com>
2024-01-22 12:14:34 +0600

    feat: mobile menu category api implemented

---
+++ src/CodeCampRestora.Api/Controllers/V1/MobileMenuCategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CodeCampRestora.Application.DTOs;
+using CodeCampRestora.Application.Models;
+using Swashbuckle.AspNetCore.Annotations;
+using CodeCampRestora.Application.Features.MobileMenuCategories.Queries;
+
+namespace CodeCampRestora.Api.Controllers.V1
+{
+    public class MobileMenuCategoryController : ApiBaseController
+    {
+        [HttpGet("GetAllHomeMenu")]
+        [SwaggerOperation(
+        Summary = "Get all menu Categories",
+        Description = @"Sample Request:
+        Get: api/v1/MenuCategory/GetAllHome"
+        )]
+        public async Task<IResult<List<MenuCategoryDto>>> GetAllHomeMenu()
+        {
+            var result = await Sender.Send(new GetAllHomeMenuCategoryQuery());
+            return result;
+        }
+    }
+}
---
+++ src/CodeCampRestora.Application/Features/MobileMenuCategories/Queries/GetAllHomeMenuCategoryQueryHandler .cs
@@ -0,0 +1,27 @@
+﻿using CodeCampRestora.Application.Common.Interfaces.MediatRs;
+using CodeCampRestora.Application.Common.Interfaces.Services;
+using CodeCampRestora.Application.DTOs;
+using CodeCampRestora.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCampRestora.Application.Features.MobileMenuCategories.Queries
+{
+    public class GetAllHomeMenuCategoryQueryHandler : IQueryHandler<GetAllHomeMenuCategoryQuery, IResult<List<MenuCategoryDto>>>
+    {
+        private readonly IMenuCategoryService _menuCategoryService;
+
+        public GetAllHomeMenuCategoryQueryHandler(IMenuCategoryService menuCategoryService)
+        {
+            _menuCategoryService = menuCategoryService;
+        }
+        public async Task<IResult<List<MenuCategoryDto>>> Handle(GetAllHomeMenuCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _menuCategoryService.GetAllHomeMenuCategoryAsync();
+            return result;
+        }
+    }
+}
