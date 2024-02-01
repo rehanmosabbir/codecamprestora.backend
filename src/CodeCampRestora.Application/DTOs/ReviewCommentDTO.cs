3b14751bf3b3ce94385de21bcbf8e7fafeb8f1c3
Jafor Iqbal <jaforiqbal5593@gmail.com>
2024-02-01 10:43:46 +0600

    fix: ReviewCommentDto Id field has added

--- src/CodeCampRestora.Application/DTOs/ReviewCommentDTO.cs
+++ src/CodeCampRestora.Application/DTOs/ReviewCommentDTO.cs
@@ -2,5 +2,7 @@
 
 public class ReviewCommentDTO
 {
+    public Guid Id { get; set; }
     public string CommentText { get; set; } = default!;
+    public bool IsCommentHidden { get; set; } = false;
 }
